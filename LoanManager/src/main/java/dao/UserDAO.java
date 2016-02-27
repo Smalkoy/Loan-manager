ad47e1da0ee1fc305a54c7b877a2a308c08d3132
Smalkoy <smalkoy@gmail.com>
2016-02-27 19:46:43 +0200

    Add UserDAO implementation

--- LoanManager/src/main/java/dao/UserDAO.java
+++ LoanManager/src/main/java/dao/UserDAO.java
@@ -4,15 +4,14 @@ import model.User;
 import java.util.Set;
 
 public interface UserDAO {
+
     public void save(User user);
 
-    public void update(User user);
+    public Set<User> findAll();
 
     public User findById(Integer userID);
 
+    public User findByName(String name);
+
     public User findByEMail(String eMail);
-
-    public Set<User> findByGroup(String group);
-
-    public Set<User> findAll();
 }
---
+++ LoanManager/src/main/java/dao/hibernate/UserDAOImpl.java
@@ -0,0 +1,79 @@
+package dao.hibernate;
+
+import dao.UserDAO;
+import model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.Set;
+
+@Repository("UserDAO")
+public class UserDAOImpl implements UserDAO {
+
+    @Autowired
+    SessionFactory sessionFactory;
+
+    @Override
+    public void save(User user) {
+        Session session = sessionFactory.openSession();
+        try {
+            session.beginTransaction();
+            session.saveOrUpdate(user);
+            session.getTransaction().commit();
+        } finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public Set findAll() {
+        Session session = sessionFactory.openSession();
+        try {
+            return (Set<User>) session.createCriteria(User.class)
+                    .addOrder(Order.asc("name"))
+                    .list();
+        } finally {
+            session.close();
+        }
+    }
+
+    public User findById(Integer userID) {
+        Session session = sessionFactory.openSession();
+        try {
+            return (User) session.createCriteria(User.class)
+                    .add(Restrictions.eq("id", userID))
+                    .list();
+        } finally {
+            session.close();
+        }
+    }
+
+
+    @Override
+    public User findByName(String userName) {
+        Session session = sessionFactory.openSession();
+        try {
+            return (User) session.createCriteria(User.class)
+                    .add(Restrictions.like("name", userName))
+                    .list();
+        } finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public User findByEMail(String email) {
+        Session session = sessionFactory.openSession();
+        try {
+            return (User) session.createCriteria(User.class)
+                    .add(Restrictions.eq("e-mail", email))
+                    .list();
+        } finally {
+            session.close();
+        }
+    }
+}
