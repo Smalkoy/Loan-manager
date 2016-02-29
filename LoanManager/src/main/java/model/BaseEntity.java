b957e223c4d807c4ceb71167d60057b35128aaa5
Smalkoy <smalkoy@gmail.com>
2016-02-29 17:13:55 +0200

    Change baseEntity

--- LoanManager/src/main/java/model/BaseEntity.java
+++ LoanManager/src/main/java/model/BaseEntity.java
@@ -1,29 +1,8 @@
 package model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-@Entity
+@MappedSuperclass
 public class BaseEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    protected Integer id;
-
-    public BaseEntity() {
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public boolean isNew() {
-        return (this.id == null);
-    }
-
+    
 }
