b9f91d5271952260a253a51dfc355ba03f57d9ee
Smalkoy <smalkoy@gmail.com>
2016-03-01 11:58:24 +0200

    Add fields creationDate and lastUpdateDate to the BaseEntity

--- LoanManager/src/main/java/model/BaseEntity.java
+++ LoanManager/src/main/java/model/BaseEntity.java
@@ -1,8 +1,35 @@
 package model;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
 
 @MappedSuperclass
 public class BaseEntity {
 
+    @Temporal(TemporalType.DATE)
+    @Column(name = "CREATION_DATE")
+    @NotNull
+    protected Date creationDate;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "LAST_UPDATE_DATE")
+    @NotNull
+    protected Date lastUpdateDate;
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getLastUpdateDate() {
+        return lastUpdateDate;
+    }
+
+    public void setLastUpdateDate(Date lastUpdateDate) {
+        this.lastUpdateDate = lastUpdateDate;
+    }
 }
